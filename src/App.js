0142ce7beed414a7794519ec59201ed704f3ca26
John Suico <johncdsuico@gmail.com>
2020-09-03 11:22:53 -0700

    add react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 import './App.css';
 import Landing from './pages/landing';
+import SignUp from './pages/signup/signUp';
 
 function App() {
   return (
-    <div className="App">
-     <Landing />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Landing />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
