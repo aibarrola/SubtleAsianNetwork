a37c58b6a4a93eb8330575aee9fdcc150a8499c6
John Suico <johncdsuico@gmail.com>
2020-09-03 17:13:56 -0700

    create signup form

--- src/pages/signup/signUp.jsx
+++ src/pages/signup/signUp.jsx
@@ -1,23 +1,52 @@
 import React from 'react';
+import './signUp.css';
 
 function signup() {
   return (
     <div className="signUp">
-      <form action="" className="signUp-form">
-        <h1 className="signup-form-title">
-          Join the Community
-          <div className="name-input-container">
-            <div className="firstName-container">
-              <label>Firstname</label>
-              <input type="text"/>
+      <div className="signUp-container">
+        <form action="" className="signUp-form">
+          <h1 className="signUp-title">Join the Community</h1>
+          <div className="signUp-form-fields">
+
+            <div className="signUp-nameFields">
+              <div className="firstName-field flex-column">
+                <label className="form-label">First Name</label>
+                <input type="text" placeholder="Tommy" className="field"/>
+              </div>
+              <div className="lastName-field flex-column">
+                <label className="form-label">Last Name</label>
+                <input type="text" placeholder="Nguyen" className="field"/>
+              </div>
             </div>
-            <div className="lastName-container">
-              <label>Lastname</label>
-              <input type="text"/>
+
+            <div className="birthday-field">
+              <label className="form-label">Birth Date</label>
+              <input type="date" placeholder="1/1/2020" className="field"/>
             </div>
+
+            <div className="email-field">
+              <label className="form-label">Email</label>
+              <input type="email" placeholder="john.g.nguyen@gmail.com" className="field"/>
+            </div>
+
+            <div className="password-field">
+              <label className="form-label">Password</label>
+              <input type="password" className="field" min="8" placeholder="8 Character Minimum"/>
+            </div>
+
+            <div className="confirm-password-field">
+              <label className="form-label">Confirm Password</label>
+              <input type="password" className="field" min="8" placeholder="8 Character Minimum"/>
+            </div>
+
+            <div className="submit-btn-container">
+              <input type="submit" value="Register" className="btn-register"/>
+            </div>
+
           </div>
-        </h1>
-      </form>
+        </form>
+      </div>
     </div>
   )
 }
