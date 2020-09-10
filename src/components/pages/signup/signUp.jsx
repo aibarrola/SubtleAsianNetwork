9beaa0c0e61498a9ba753b4d130cdc96f4942dcd
James Nguyen <nguyen.james615@gmail.com>
2020-09-10 13:03:07 -0700

    updated signUp page to get data from form

--- src/components/pages/signup/signUp.jsx
+++ src/components/pages/signup/signUp.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import "./signUp.css";
 
-function signup() {
-  return (
+
+
+
+
+
+
+
+export default class SignUp extends React.Componenet {
+  constructor(props){
+    super(props);
+    this.state ={
+      firstName: null,
+      lastName: null,
+      email: null,
+      password: null,
+      confirmPassword: null
+    }
+  }
+  handleChange = (event) =>{
+    event.preventDefault();
+    const {name, value} = event.target;//gets the name and vlaue of the specific event 
+    //ie. if something was entered in firstName, firstName is returned as name and value of the firstname
+
+    //handle validation here
+
+    
+    this.setState({[name]: value}) 
+  }
+  render(){ 
+    return(
     <div className="signUp">
       <div className="signUp-container">
         <form action="" className="signUp-form">
@@ -11,45 +39,50 @@ function signup() {
             <div className="signUp-nameFields">
               <div className="firstName-field flex-column">
                 <label className="form-label">First Name</label>
-                <input type="text" placeholder="Tommy" className="field" />
+                <input type="text" name = "firstName" placeholder="Tommy" className="field" onChange = {this.handleChange}/>
               </div>
               <div className="lastName-field flex-column">
                 <label className="form-label">Last Name</label>
-                <input type="text" placeholder="Nguyen" className="field" />
+                <input type="text" placeholder="Nguyen" name = "lastName" className="field" onChange = {this.handleChange}/>
               </div>
             </div>
 
             <div className="birthday-field">
               <label className="form-label">Birth Date</label>
-              <input type="date" placeholder="1/1/2020" className="field" />
+              <input type="date" placeholder="1/1/2020" className="field" name = "birthdate"onChange = {this.handleChange}/>
             </div>
 
             <div className="email-field">
               <label className="form-label">Email</label>
               <input
+                name = "email"
                 type="email"
                 placeholder="john.g.nguyen@gmail.com"
                 className="field"
-              />
+                onChange = {this.handleChange}/>
             </div>
 
             <div className="password-field">
               <label className="form-label">Password</label>
               <input
+                name = "password"
                 type="password"
                 className="field"
                 min="8"
                 placeholder="8 Character Minimum"
+                onChange = {this.handleChange}
               />
             </div>
 
             <div className="confirm-password-field">
               <label className="form-label">Confirm Password</label>
               <input
+                name = "confirmPassword"
                 type="password"
                 className="field"
                 min="8"
                 placeholder="8 Character Minimum"
+                onChange = {this.handleChange}
               />
             </div>
 
@@ -62,5 +95,4 @@ function signup() {
     </div>
   );
 }
-
-export default signup;
+}
