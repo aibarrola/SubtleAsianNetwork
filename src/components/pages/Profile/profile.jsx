b522d897a4a013e2facd95ffa043cf5da5dbd114
Angelo Ibarrola <vino_1105@hotmail.com>
2020-09-10 10:46:30 -0700

    added sidebar component

--- src/components/pages/Profile/profile.jsx
+++ src/components/pages/Profile/profile.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import './profile'
+import './profile.css'
 import Navbar from '../../navbar'
+import Sidebar from '../../sidebar'
 
 function profile(){
     return(
-        <section className="profileContainer">
+        <section>
             <Navbar />
-            <div>
-                <h1> Profile </h1>
+            <div className="profileContainer">
+                <div className="sidebar">
+                    <Sidebar />
+                </div>
+                <div className="profileMain">
+                    <h1> Profile </h1>
+                </div>
             </div>
         </section>
     )
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './sidebar.css'
+
+function sidebar(){
+    return(
+        <section>
+            <div>
+                <h1> Sidebar </h1>
+            </div>
+        </section>
+    )
+}
+
+export default sidebar;
