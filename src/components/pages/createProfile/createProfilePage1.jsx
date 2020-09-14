c91000eda8feec6195771715a972236e582a85c7
John Suico <johncdsuico@gmail.com>
2020-09-14 11:04:44 -0700

    update createProfile component with backend connection

--- src/components/pages/createProfile/createProfilePage1.jsx
+++ src/components/pages/createProfile/createProfilePage1.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from 'react';
+import Axios from 'axios';
 import "./createProfile.css";
 
-function createProfilePage1() {
+function CreateProfilePage1(props) {
+
+  const [school, setSchool] = useState('');
+  const [profession, setProfession] = useState('');
+  const [ethnicity, setEthnicity] = useState('');
+
+  function schoolChange(e) {
+    setSchool(e.target.value);
+  }
+  function professionChange(e) {
+    setProfession(e.target.value);
+  }
+  function ethnicityChange(e) {
+    setEthnicity(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const updateOne = {
+      school: school,
+      profession: profession,
+      ethnicity: ethnicity
+    }
+
+    Axios.post('http://localhost:5000/updateProfile/1/:userID', updateOne)
+      .then(res => { 
+        console.log(res.data);
+        window.location.replace('/createProfile2');
+      })
+      .catch(err => console.oog("Error: " + err));
+
+  }
+
   return (
     <div className="createProfile">
       <div className="createProfile-container">
@@ -18,20 +52,20 @@ function createProfilePage1() {
           <img src="/images/createProfile/ProgressBar1.svg" alt="" className="progressBar"/>
         </div>
 
-        <form action="" className="createProfile-form">
+        <form className="createProfile-form" onSubmit={handleSubmit}>
           <div className="createProfile-form-field">
             <label className="createProfile-form-label">School:</label>
-            <input type="text" placeholder="E.g. San Jose State University" className="createProfile-form-input"/>
+            <input type="text" placeholder="E.g. San Jose State University" className="createProfile-form-input" value={school} onChange={schoolChange} required/>
           </div>
 
           <div className="createProfile-form-field">
             <label className="createProfile-form-label">Profession:</label>
-            <input type="text" placeholder="E.g. Student, Software Engineer" className="createProfile-form-input"/>
+            <input type="text" placeholder="E.g. Student, Software Engineer" className="createProfile-form-input" value={profession} onChange={professionChange} required/>
           </div>
 
           <div className="createProfile-form-field">
             <label className="createProfile-form-label">Ethnicity:</label>
-            <input type="text" placeholder="E.g. Vietnamese" className="createProfile-form-input"/>
+            <input type="text" placeholder="E.g. Vietnamese" className="createProfile-form-input" value={ethnicity} onChange={ethnicityChange}/>
           </div>
 
           <div className="createProfile-btn-container"> 
@@ -44,4 +78,4 @@ function createProfilePage1() {
   );
 }
 
-export default createProfilePage1;
+export default CreateProfilePage1;
--- src/components/pages/createProfile/createProfilePage2.jsx
+++ src/components/pages/createProfile/createProfilePage2.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from 'react';
+import Axios from 'axios';
 import "./createProfile.css";
 
-function createProfilePage1() {
+function CreateProfilePage1() {
+
+  const [location, setLocation] = useState('');
+  const [interests, setInterests] = useState('');
+  const [bio, setBio] = useState('');
+
+  function locationChange(e) {
+    setLocation(e.target.value);
+  }
+  function interestsChange(e) {
+    setInterests(e.target.value);
+  }
+  function bioChange(e) {
+    setBio(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const updateOne = {
+      location: location,
+      interests: interests,
+      bio: bio
+    }
+
+    Axios.post('http://localhost:5000/updateProfile/2/:userID', updateOne)
+      .then(res => { 
+        console.log(res.data);
+      })
+      .catch(err => console.oog("Error: " + err));
+
+  }
+
   return (
     <div className="createProfile">
       <div className="createProfile-container">
@@ -18,21 +51,21 @@ function createProfilePage1() {
           <img src="/images/createProfile/ProgressBar2.svg" alt="" className="progressBar"/>
         </div>
 
-        <form action="" className="createProfile-form">
+        <form className="createProfile-form" onSubmit={handleSubmit}>
           <div className="createProfile-form-field">
             <label className="createProfile-form-label">Location:</label>
-            <input type="text" placeholder="E.g. San Jose State University" className="createProfile-form-input"/>
+            <input type="text" placeholder="E.g. San Jose State University" className="createProfile-form-input" value={location} onChange={locationChange}/>
           </div>
 
           <div className="createProfile-form-field">
             <label className="createProfile-form-label">Interest:</label>
-            <input type="text" placeholder="E.g. Student, Software Engineer" className="createProfile-form-input"/>
+            <input type="text" placeholder="E.g. Student, Software Engineer" className="createProfile-form-input" value={interests} onChange={interestsChange}/>
           </div>
 
           <div className="createProfile-form-field">
             <label className="createProfile-form-label">Bio:</label>
             <textarea 
-              placeholder="Separate interests with a comma. E.g. STONKS, money, love" className="createProfile-textArea"></textarea>
+              placeholder="Separate interests with a comma. E.g. STONKS, money, love" className="createProfile-textArea" value={bio} onChange={bioChange}></textarea>
           </div>
 
           <div className="createProfile-btn-container"> 
@@ -45,4 +78,4 @@ function createProfilePage1() {
   );
 }
 
-export default createProfilePage1;
+export default CreateProfilePage1;
