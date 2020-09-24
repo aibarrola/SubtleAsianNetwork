68364596dc28eebb31498432b16b1786acf0b241
Tommy Kien Huynh <TommyKienHuynh@gmail.com>
2020-09-24 10:42:50 -0700

    fixed bugs on app.js and pushed fake data on networking page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Landing from "./components/pages/landing";
 import SignUp from "./components/pages/signup/signUp";
-import Profile from "./components/pages/Profile/Profile";
-import CreateProfilePage1 from "./components/pages/createProfile/CreateProfilePage1";
+import Profile from "./components/pages/Profile/profile";
+import CreateProfilePage1 from "./components/pages/createProfile/createProfilePage1";
 import Networking from "./components/pages/Networking/networking";
-import CreateProfilePage2 from "./components/pages/createProfile/CreateProfilePage2";
+import CreateProfilePage2 from "./components/pages/createProfile/createProfilePage2";
 import LogIn from "./components/pages/login/login";
 
 function App() {
@@ -28,7 +28,7 @@ function App() {
         <Route path="/networking">
           <Networking />
         </Route>
-
+        <Route path="/createProfile1"></Route>
         <Route path="/user/:id/cp/1">
           <CreateProfilePage1 />
         </Route>
