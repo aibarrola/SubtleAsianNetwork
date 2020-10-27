c57dd3c87eb1ac85975a5c6416e290aac10b0eb8
Angelo Ibarrola <vino_1105@hotmail.com>
2020-10-27 14:04:59 -0700

    added profile class names for css

--- src/components/pages/connectSection.jsx
+++ src/components/pages/connectSection.jsx
@@ -7,7 +7,7 @@ function render() {
     <div className="connectSection">
       <img src={connectSVG} className="connect" alt=""/>
       <div className="connectInfoContainer">
-        <h2> Connect with Others </h2>
+        <h2 className="mainSectionHeader"> Connect with Others </h2>
         <p>
           Learn about other people in the Kickstarter program and expand your network. You will also get to see other groups and learn about what they are currently doing by asking questions or simply viewing their progress. 
         </p>
--- src/components/pages/opportunitiesSection.jsx
+++ src/components/pages/opportunitiesSection.jsx
@@ -6,9 +6,9 @@ function render() {
   return (
     <div className="opportunitiesSection">
       <div className="connectInfoContainer">
-        <h2> Track Progress </h2>
+        <h2 className="mainSectionHeader"> Track Team Progress </h2>
         <p>
-          You will be able to edit and customize your own team page. This inludes the team name, description, and more! This will be your main hub for sending out announcements, asking questions, and a task list. All customized for you to make it as easy as possible to create an efficient workflow for your project!
+          You will be able to edit and customize your own team page. This will be your main hub for sending out announcements, asking questions, and assigning tasks. All customized for you to make it as easy as possible to create an efficient workflow for your project!
         </p>
       </div>
       <img src={opportunitiesSVG} className="connect" alt=""/>
