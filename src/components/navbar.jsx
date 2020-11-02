ae8652f5e828d76dca10194166d125680dfc72cc
John Suico <johncdsuico@gmail.com>
2020-11-02 07:11:03 -0800

    add myprofile to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 import './navbar.css'
 
+import { getJwt } from "./Helpers/jwt";
+
 function Navbar(){
+
+    const [token, setToken] = useState();
+
+    let retrieved = localStorage.getItem('user');
+    retrieved = JSON.parse(retrieved);
+
+    const [user, setUser] = useState(retrieved.user_id);
+
+    useEffect(() => {
+        setToken(getJwt());
+    }, [])
+
     return(
         <section>
             <div class="navbar">
                 <a href="/networking"> Network </a>
                 <a href="/group"> Groups </a>
-                {/* <a href="Profile"> Profile </a> */}
+                <a href={`/user/profile/${user}`}> My Profile </a>
                 {/* <a href="Home"> Home </a> */}
             </div>
         </section>
