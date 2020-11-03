06bc9a6dd23f5f4d9cff14a2581081cba4ed3a3d
James Nguyen <nguyen.james615@gmail.com>
2020-11-02 18:28:00 -0800

    hardcoded groups into createProfilePage

--- src/components/pages/createProfile/createProfilePage2.jsx
+++ src/components/pages/createProfile/createProfilePage2.jsx
@@ -12,6 +12,7 @@ function CreateProfilePage1() {
   const [interests, setInterests] = useState('');
   const [bio, setBio] = useState('');
   const [token, setToken] = useState();
+  const [team, setTeam] = useState('5fa0be6605f4aa470804a2cc');
 
   let {id} = useParams();
   let userString = localStorage.getItem('user');
@@ -30,22 +31,32 @@ function CreateProfilePage1() {
   function bioChange(e) {
     setBio(e.target.value);
   }
-
+  const groupChange =(e)=>{
+    setTeam(e.target.value);
+  }
   function handleSubmit(e) {
     e.preventDefault();
 
     const updateOne = {
       location: location,
       interests: interests,
-      bio: bio
+      bio: bio,
+      group: team
     }
-
+    const user = {
+      userId: id
+    }
     const config = {
       headers : {
         'x-auth-token': token
       }
     }
-
+    
+    Axios.post(`https://san-api.herokuapp.com/groups/user/${team}/add`,user)
+    .then(res=>{
+      console.log(res.data);
+    })
+    .catch(err=> console.log("Error " + err.msg))
     // Axios.post(`http://localhost:5000/users/${id}/createprofile/2`, updateOne, config)
     Axios.post(`https://san-api.herokuapp.com/users/${id}/createprofile/2`, updateOne, config)
       .then(res => { 
@@ -54,6 +65,7 @@ function CreateProfilePage1() {
       })
       .catch(err => console.log("Error: " + err.msg));
 
+      
   }
 
   return (
@@ -89,6 +101,18 @@ function CreateProfilePage1() {
               placeholder="Tell me about yourself!" className="createProfile-textArea" value={bio} onChange={bioChange}></textarea>
           </div>
 
+          <div className="createProfile-form-field">
+          <label className="createProfile-form-label">Team:</label>
+            <select className="createProfile-form-input" onChange={groupChange}>
+              <option value="5fa0be6605f4aa470804a2cc">Team App A</option>
+              <option value = "5fa0b106cfead2209075a991">Team App B</option>
+              <option value ="5fa0b10ccfead2209075a992">Team App C</option>
+              <option value = "5fa0b15acfead2209075a994">Team Web A</option> 
+              <option value = "5fa0b15fcfead2209075a995">Team Web B</option> 
+              <option value = "5fa0b165cfead2209075a996" >Team Web C</option> 
+              <option value ="5fa0b180cfead2209075a997">Discord</option>
+            </select>
+          </div>
           <div className="createProfile-btn-container"> 
             <input type="submit" className="btn-continue" value="Continue"/>
           </div>
