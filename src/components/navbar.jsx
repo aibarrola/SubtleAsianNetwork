2ca5af83ac40f8b8b007a7b46f2ab2dc059867ac
John Suico <johncdsuico@gmail.com>
2021-02-11 09:46:26 -0800

    Add comments to explain code

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,33 +6,42 @@ import UnLoggedNav from "./unLoggedNavbar";
 import LoggedNav from "./loggedNav";
 
 function Navbar() {
+
+  // React hooks to store values and states
   const [token, setToken] = useState();
-
-  let retrieved = localStorage.getItem("user");
-  retrieved = JSON.parse(retrieved);
-
   const [user, setUser] = useState(retrieved);
   const [group, setGroup] = useState("");
   const [showNav, setNavBar] = useState(false);
   const [loggedInNav, setloggedIn] = useState(false);
 
+  // Retrieve user information from local storage
+  let retrieved = localStorage.getItem("user");
+  // Turn single line of text into usable JSON
+  retrieved = JSON.parse(retrieved);
+
+  // History hook for use of page redirection
   let history = useHistory(); 
 
-   useEffect(() => {
+  // Do the following upon page load up
+  useEffect(() => {
+    // Check if there is a 'user' and 'token' var in browser storage
     if((localStorage.getItem('user') && localStorage.getItem('token'))){ 
-    setloggedIn(true);
-    setToken(getJwt());
-    Axios.get(`https://san-api.herokuapp.com/users/${user.user_id}`).then(
-      (response) => {
+      // If true then set login hook to be true
+      setloggedIn(true);
+      // Set token hook to JWT
+      setToken(getJwt());
+      // Send get request to get user information
+      Axios.get(`https://san-api.herokuapp.com/users/${user.user_id}`).then(response => {
+        // Set user hook to user data
         setUser(response.data);
+        // set group to user's group
         setGroup(response.data.group);
       }
     );}
   }); 
 
- 
 
-   const changeBackground = () => {
+  const changeBackground = () => {
     if (window.scrollY >= 100) {
       setNavBar(true);
     } else {
@@ -42,11 +51,12 @@ function Navbar() {
 
   window.addEventListener("scroll", changeBackground); 
 
-  if(loggedInNav){
+  // If user is logged in, then display logged in navbar
+  if(loggedInNav) {
     return (
       <LoggedNav user_id = {user._id} group = {group}/>
       );
-  }else{
+  } else {
     return (<UnLoggedNav/>);
   }
  
