522fdb99bd7d1f58dab5d0e5ca3a762f4c3a344a
John Suico <johncdsuico@gmail.com>
2021-02-11 09:36:47 -0800

    Add comments to explain code

--- src/components/pages/createProfile/createProfilePage1.jsx
+++ src/components/pages/createProfile/createProfilePage1.jsx
@@ -8,21 +8,27 @@ import { getJwt } from "../../Helpers/jwt";
 
 function CreateProfilePage1() {
 
+  // React hooks to hold values
   const [school, setSchool] = useState('');
   const [profession, setProfession] = useState('');
   const [ethnicity, setEthnicity] = useState('');
   const [token, setToken] = useState();
-  const [error, setError] = useState("");
+  // const [error, setError] = useState("");
 
+  // Get user id from the url parameters
   let {id} = useParams();
+
+  // Get information from user token and store in user var
   let userString = localStorage.getItem('user');
   let user = JSON.parse(userString);
 
   
+  // Once page load get JWT and set Token
   useEffect(() => {
     setToken(getJwt());
   }, []);
 
+  // Set vars to their corresponding value depending on their field
   function schoolChange(e) {
     setSchool(e.target.value);
   }
@@ -33,35 +39,45 @@ function CreateProfilePage1() {
     setEthnicity(e.target.value);
   }
 
+  // Submit button handling
   function handleSubmit(e) {
+    // Prevent page from reloading upon submission
     e.preventDefault();
 
+    // Store values into updateOne JSON
     const updateOne = {
       school: school,
       profession: profession,
       ethnicity: ethnicity
     }
 
+    // Create headers
     const config = {
       headers : {
         'x-auth-token': token
       }
     }
 
-    // Axios.post(`http://localhost:5000/users/${id}/createprofile/1`, updateOne, config)
+    // Send post request upon submission to backend
     Axios.post(`https://san-api.herokuapp.com/users/${id}/createprofile/1`, updateOne, config)
       .then(res => { 
+        // Console log the data for debugging
         console.log(res.data);
+
+        // When done, go to next step in create profile
         window.location.assign(`/user/${id}/cp/2`);
       })
+      // If there is an error, console log it
       .catch(err => console.log("Error: " + err));
 
   }
 
+  // Render this HTMl code on the webpage
   return (
     <div className="createProfile">
       <div className="createProfile-container">
 
+        {/* Profile picture container */}
         <div className="profilePicture">
           <img src="/images/createProfile/profilepic.svg" alt="Profile Default" className="profilePictureImg"/>
         </div>
@@ -76,21 +92,26 @@ function CreateProfilePage1() {
         </div>
 
         <form className="createProfile-form" onSubmit={handleSubmit}>
+
+          {/* School field */}
           <div className="createProfile-form-field">
             <label className="createProfile-form-label">School</label>
             <input type="text" placeholder="E.g. San Jose State University" className="createProfile-form-input" value={school} onChange={schoolChange} required/>
           </div>
 
+          {/* Profession field */}
           <div className="createProfile-form-field">
             <label className="createProfile-form-label">Profession</label>
             <input type="text" placeholder="E.g. Computer Science Student, Software Engineer Intern" className="createProfile-form-input" value={profession} onChange={professionChange} required/>
           </div>
 
+          {/* Ethnicity field */}
           <div className="createProfile-form-field">
             <label className="createProfile-form-label">Ethnicity</label>
             <input type="text" placeholder="E.g. Vietnamese" className="createProfile-form-input" value={ethnicity} onChange={ethnicityChange}/>
           </div>
 
+          {/* Submit button */}
           <div className="createProfile-btn-container"> 
             <input type="submit" className="btn-continue" value="Continue"/>
           </div>
