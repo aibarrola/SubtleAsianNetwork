3c27ffbeed69e31d8ec20ee1016751f778126fa3
cmtnguyen <66109485+cmtnguyen@users.noreply.github.com>
2021-03-24 00:05:03 -0700

    edited landing

--- src/components/pages/landing/trackProgress/TrackProgress.jsx
+++ src/components/pages/landing/trackProgress/TrackProgress.jsx
@@ -2,21 +2,55 @@ import React from 'react';
 
 import './trackProgress.css'
 
-import progressSVG from './progressSVG.svg';
+import formSVG from './formSVG.svg';
+import shareSVG from './shareSVG.svg';
+import groupSVG from './groupSVG.svg';
 
 function TrackProgress() {
   return (
     <div className="progress">
+      <h2 className="progress-title">How it works</h2>
       <div className="progress-container">
+        <img src={formSVG} className="sizeSVG" alt="Create Form Graphic"/>
         <div className="progress-info-container">
-          <h2 className="progress-title">Track Team Progress</h2>
+          <p className="progress-subtitle">
+          Create a form
+          </p>
           <p className="progress-info">
-          You will be able to edit and customize your own team page. Create an efficient workflow for your group members through announcements and a to-do list
+          Set up is easy. Let Jade know what kind of questions you want to ask your group members.
           </p>
         </div>
-        <img src={progressSVG} className="progressSVG" alt="Progress Section Graphic"/>
+        
       </div>
+      <div className="progress-container2">
+        <img src={shareSVG} className="sizeSVG" alt="Create Form Graphic"/>
+        <div className="progress-info-container">
+          <p className="progress-subtitle">
+          Share your link
+          </p>
+          <p className="progress-info">
+          Share your Jade link to the group members so they can fill our your form.
+          </p>
+        </div>
+        
+      </div>
+
+      <div className="progress-container3">
+        <img src={groupSVG} className="sizeSVG" alt="Create Group Graphic"/>
+        <div className="progress-info-container">
+          <p className="progress-subtitle">
+          Create your group
+          </p>
+          <p className="progress-info">
+          Once group members are finished, you can efficiently put them in groups.
+          </p>
+        </div>
+        
+      </div>
+
     </div>
+
+    
   )
 }
 
